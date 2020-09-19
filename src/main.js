e17c6674a51d68e4992b0768d7f59b512a872d86
zlotyli <1984320939@qq.com>
2020-09-19 20:13:35 +0800

    fastclick以及图片懒加载和px2vw-css

--- src/main.js
+++ src/main.js
@@ -5,9 +5,19 @@ import store from './store';
 // 导入插件
 import toast from 'components/common/toast'
 
+// 导入fastclick
+import FastClick from 'fastclick';
+// 导入懒加载lazyload
+import VueLazyLoad from 'vue-lazyload';
+
 Vue.config.productionTip = false
 
-
+// 解决移动端300ms的延迟
+FastClick.attach(document.body);
+// 使用懒加载的插件
+Vue.use(VueLazyLoad,{
+  loading: require('assets/img/common/placeholder.png')
+});
 // 添加事件总线对象
 Vue.prototype.$bus = new Vue()
 
